cfc2a4c8379c7fbef5f1aaba98a8707e29f4adfe
Yong Li <yong.li@intel.com>
2024-02-19 16:29:13 +0800

    lib: sbi: Add support to update domain context entry
    Some of the domain os needs to update the context entry
    dynamically, e.g. OP-TEE.
    
    The patch provides an addition api to support update
    the domain context entry.
    
    Signed-off-by: Yong Li <yong.li@intel.com>
    Tested-by: Yu Chien Peter Lin <peterlin@andestech.com>
    Reviewed-by: Yu Chien Peter Lin <peterlin@andestech.com>

--- include/sbi/sbi_domain_context.h
+++ include/sbi/sbi_domain_context.h
@@ -12,6 +12,16 @@
 struct sbi_domain;
 
 /**
+ * Set domain entry point
+ * @param dom pointer to domain
+ * @param entry_point new entry point of domain
+ *
+ * @return 0 on success and negative error code on failure
+ */
+int sbi_domain_context_set_mepc(struct sbi_domain *dom, unsigned long entry_point);
+
+
+/**
  * Enter a specific domain context synchronously
  * @param dom pointer to domain
  *
