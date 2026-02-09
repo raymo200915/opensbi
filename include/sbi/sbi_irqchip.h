17013c6b5b93540f7da00a1722f8bbbc2832cc83
Raymond Mao <raymond.mao@riscstar.com>
2026-02-09 16:39:12 -0500

    lib: sbi_irqchip: Add irqchip private context pointer in sbi_irqchip_device
    HW specific private data is required for irqchip device operations,
    for example, for APLIC, address, num_idc and num_source are needed
    for mask / unmask / claim an IRQ, thus it is reasonable to add a
    private HW data pointer in sbi_irqchip_device.
    
    Signed-off-by: Raymond Mao <raymond.mao@riscstar.com>

--- include/sbi/sbi_irqchip.h
+++ include/sbi/sbi_irqchip.h
@@ -56,6 +56,9 @@ struct sbi_irqchip_device {
 
 	/** Unmask a hardware interrupt of this irqchip */
 	void (*hwirq_unmask)(struct sbi_irqchip_device *chip, u32 hwirq);
+
+	/** Irqchip device private context */
+	void *chip_priv;
 };
 
 /**
